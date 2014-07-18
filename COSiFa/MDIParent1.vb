ac3b6880f8752e9d8aa08fcf75faca368e86a488
Paride Balestri IPREL <paride_balestri@iprel.it>
2014-07-18 15:03:04 +0200

    2014-07-18: lettura da MSSQL funzionante

--- COSiFa/MDIParent1.vb
+++ COSiFa/MDIParent1.vb
@@ -1,21 +1,45 @@
 ﻿Imports System.Windows.Forms
+Imports System.Data.SqlClient
 
 Public Class MDIParent1
 
 
     Private Sub ExitToolsStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ExitToolStripMenuItem.Click
-        Me.Close()
+        'Me.Close()
+
+        End
     End Sub
 
 
     Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+
         MsgBox("System.Environment.UserName = " + System.Environment.UserName.ToString + vbCrLf + "System.Environment.UserDomainName = " + System.Environment.UserDomainName.ToString, vbOKOnly)
+        MsgBox("My.Resources.String1 = " + My.Resources.connectionString, vbOKOnly)
+
     End Sub
+    Dim userlevel As Integer = 0
+    Dim cn As SqlClient.SqlConnection = New SqlClient.SqlConnection(My.Resources.connectionString)
 
     Private Sub MDIParent1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Inserire qui le attività da fare al caricamento
         'tipo lettura dei diritti utente e abilitazione varie funzioni
 
+        Dim sqlstring As String = "SELECT userlevel FROM tUser WHERE username='" + System.Environment.UserName + "';"
+        cn.Open()
+        If cn.State = ConnectionState.Open Then
+            Dim sqlcmd As New SqlCommand(sqlstring, cn)
+            Dim sqlReader As SqlDataReader = sqlcmd.ExecuteReader()
+            While sqlReader.Read()
+                Debug.Print("******************* " + "{0}", sqlReader(0))
+            End While
+            sqlReader.Close()
+            cn.Close()
+        End If
 
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.Show()
     End Sub
 End Class
--- COSiFa/My Project/Application.Designer.vb
+++ COSiFa/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.COSiFa.MDIParent1
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.COSiFa.SplashScreen1
+        End Sub
     End Class
 End Namespace
